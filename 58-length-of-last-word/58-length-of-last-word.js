e2a3bf9b8e2482dfda588ac0f5878589035aae3d
sally-choirm <105703051+sally-choirm@users.noreply.github.com>
2022-05-30 22:53:47 +0900

    Time: 67 ms (73.01%), Space: 42.3 MB (22.79%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -5,6 +5,5 @@
 var lengthOfLastWord = function(s) {
     let splitS = s.split(" ");
     let filterS = splitS.filter((data)=> data!='');
-    console.log(filterS)
     return filterS[filterS.length-1].length;
 };
