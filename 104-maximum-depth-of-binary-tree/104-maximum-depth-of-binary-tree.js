3e1fc2f465244e064d1d5d3e4a9e0c7759724ce2
sally <105703051+sally-choirm@users.noreply.github.com>
2022-06-02 13:10:22 +0900

    Time: 129 ms (8.74%), Space: 45.6 MB (23.31%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -13,7 +13,7 @@
 
 
 var maxDepth = function(root) {
-    if(root === null || root ===undefined) return 0;
+    if(root === null) return 0;
     
     return Math.max(maxDepth(root.left), maxDepth(root.right))+1
 }
