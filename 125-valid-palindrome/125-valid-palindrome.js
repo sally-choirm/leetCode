cbd1a900a1e27452f7cee587553373eb40b8d170
sally <105703051+sally-choirm@users.noreply.github.com>
2022-06-03 13:36:05 +0900

    Time: 294 ms (5.03%), Space: 51.6 MB (8.62%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let ss =''
+
+    for(let i=0;i<s.length;i++){
+        let x = s[i];
+        let ascii_code = x.charCodeAt(0);
+        if(ascii_code>=47&&ascii_code<=57){
+                        ss+=x;
+
+        }
+        if(ascii_code>=65&&ascii_code<=90){
+            ss+=x.toLowerCase();
+        } else if(ascii_code>=97&&ascii_code<=122){
+            ss+=x;
+        }
+    }
+    console.log(ss);
+    for(let i=0;i<parseInt(ss.length/2);i++){
+            console.log(ss[i],ss[ss.length-1]);
+
+        if(ss[i]!=ss[ss.length-i-1]) return false;
+    }
+    return true;
+};
