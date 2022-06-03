6501ff943d362df9e81a427cd68071a7cb119b0e
sally <105703051+sally-choirm@users.noreply.github.com>
2022-06-03 13:36:22 +0900

    Time: 92 ms (61.61%), Space: 46 MB (50.23%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -9,8 +9,7 @@ var isPalindrome = function(s) {
         let x = s[i];
         let ascii_code = x.charCodeAt(0);
         if(ascii_code>=47&&ascii_code<=57){
-                        ss+=x;
-
+            ss+=x;
         }
         if(ascii_code>=65&&ascii_code<=90){
             ss+=x.toLowerCase();
@@ -18,10 +17,7 @@ var isPalindrome = function(s) {
             ss+=x;
         }
     }
-    console.log(ss);
     for(let i=0;i<parseInt(ss.length/2);i++){
-            console.log(ss[i],ss[ss.length-1]);
-
         if(ss[i]!=ss[ss.length-i-1]) return false;
     }
     return true;
