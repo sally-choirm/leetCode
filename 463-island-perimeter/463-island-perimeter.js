abd7117577a5a7d003e4b796d135cb4b0fb4bdbd
sally <105703051+sally-choirm@users.noreply.github.com>
2022-07-15 13:23:50 +0900

    Time: 169 ms (86.85%), Space: 51.2 MB (32.18%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.js
+++ 463-island-perimeter/463-island-perimeter.js
@@ -7,12 +7,11 @@ var islandPerimeter = function(grid) {
     for(let i=0;i<grid.length;i++){
         for(let j=0;j<grid[i].length;j++){
             if(grid[i][j]){
-                if(!grid[i][j+1]) result++;
-                if(!grid[i][j-1]) result++;
-                if(!grid[i-1] || !grid[i-1][j]) result++;
-                if(!grid[i+1] || !grid[i+1][j]) result++;
+                if(!grid[i][j+1]) result++; // right
+                if(!grid[i][j-1]) result++; // left
+                if(!grid[i-1] || !grid[i-1][j]) result++; // top
+                if(!grid[i+1] || !grid[i+1][j]) result++; // bottom
             }
-           
         }
     }
     return result;
