2e745143832723df5e6262797e892d2299d069f2
sally <105703051+sally-choirm@users.noreply.github.com>
2022-07-19 09:38:17 +0900

    Time: 323 ms (7.74%), Space: 50.7 MB (5.05%) - LeetHub

---
+++ 506-relative-ranks/506-relative-ranks.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    let rank = ["Gold Medal","Silver Medal","Bronze Medal"];
+    let arr = [...score];
+    let result =[];
+    arr.sort((a,b)=>b-a);
+    console.log(arr, score);
+    for(let i=0;i<arr.length;i++){
+        let idx = score.indexOf(arr[i]);
+        console.log('id-',idx);
+        if(i<3) result[idx] = rank[i];
+        else result[idx]=String(i+1);
+    }
+    return result;
+};
