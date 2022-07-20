ae6c662b29f661785c2aa63c7e2ccb80ee3a63a5
sally <105703051+sally-choirm@users.noreply.github.com>
2022-07-20 13:06:30 +0900

    Time: 81 ms (72.16%), Space: 42.6 MB (22.51%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    let capitalStatus =false;
+    let capitalCount=0;
+    for(let i=0;i<word.length;i++){
+        if(word[i]>='A' && word[i]<='Z'){
+            if(capitalStatus){
+                return false; // 소문자가 나오다가 대문자가 나올경우 
+            }
+            else capitalCount++;
+        }
+        else{
+             capitalStatus = true;
+        }
+    }
+    return capitalCount <= 1 ||  !capitalStatus;
+};
