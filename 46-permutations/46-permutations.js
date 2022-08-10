3de49e6656d6bdce53df937d8676885b249b12f2
sally <105703051+sally-choirm@users.noreply.github.com>
2022-08-10 18:38:11 +0900

    Time: 121 ms (34.84%), Space: 46.7 MB (7.23%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function permutations(arr, n) {
+  if (n === 1) return arr.map((v) => [v]);
+  let result = [];
+
+  arr.forEach((fixed, idx, arr) => {
+    const rest = arr.filter((_, index) => index !== idx);
+    const perms = permutations(rest, n - 1);
+    const combine = perms.map((v) => [fixed, ...v]);
+    result.push(...combine);
+  });
+
+  return result;
+}
+var permute = function(nums) {
+    return permutations(nums,nums.length);
+};
