00740294aebbe6aff61bf9ee42fc86f387206851
sally <105703051+sally-choirm@users.noreply.github.com>
2022-08-10 13:53:53 +0900

    Time: 275 ms (8.94%), Space: 61.6 MB (5.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -3,7 +3,41 @@
  * @param {number} k
  * @return {number}
  */
+
+class MaxQueue{
+    constructor(){
+        this.heap=[null];
+    }
+       push(value) {
+        this.heap.push(value);
+        let currentIndex = this.heap.length - 1;
+        let parentIndex = Math.floor(currentIndex / 2);
+
+        while (parentIndex !== 0 && this.heap[parentIndex] <= value) {
+          const temp = this.heap[parentIndex];
+          this.heap[parentIndex] = value;
+          this.heap[currentIndex] = temp;
+          currentIndex = parentIndex;
+          parentIndex = Math.floor(currentIndex / 2);
+        }
+   }
+}
 var findKthLargest = function(nums, k) {
-    nums= nums.sort((a,b)=>b-a);
-    return nums[k-1];
+    // let queue = new MaxQueue();
+    // for(let num of nums){
+    //     queue.push(num);
+    // }
+    // console.log(queue.heap);
+    // return queue.heap[k];
+     let pq = new MinPriorityQueue();
+
+    for (let x of nums) {
+        pq.enqueue(x);
+        if (pq.size() > k) {
+            pq.dequeue();
+        }
+    }
+
+    return pq.front().element;
+    
 };
