4d07549d9e462652e57e897218354d891c93dc43
sally <105703051+sally-choirm@users.noreply.github.com>
2022-08-11 09:47:27 +0900

    Time: 155 ms (5.09%), Space: 48.9 MB (5.32%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const result=[];
+    for(const m of matrix) result.push([]);
+    for(let i=0;i<matrix.length;i++){
+            console.log('aa=',result);
+            for(let j=matrix[i].length-1;j>=0;j--){
+                result[i].push(matrix[j][i]);
+            }
+        }
+    for(let i=0;i<matrix.length;i++){
+        matrix[i]=result[i];
+    }
+};
