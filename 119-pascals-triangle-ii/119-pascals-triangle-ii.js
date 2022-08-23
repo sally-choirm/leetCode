eb1976aa685f0f877bb7d30be3b9742dd7f6791a
sally <105703051+sally-choirm@users.noreply.github.com>
2022-08-23 09:49:29 +0900

    Time: 114 ms (12.81%), Space: 44.2 MB (6.35%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -13,14 +13,11 @@ var getRow = function(rowIndex) {
             for(let j=0;j<=rowIndex;j++){
                 if(j==0 || j==i) arr1.push(1);
                 else {
-
                     let val = arr[i-1][j-1]+arr[i-1][j];
-                    console.log(i,j,rowIndex,val);
                     arr1.push(val);
                 }
             }
             arr.push(arr1);
-            console.log(arr);
         }
     }
     return arr[rowIndex];
