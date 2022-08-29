45ae69a8de894a99ed5960a0f40253e8912fbee0
sally <105703051+sally-choirm@users.noreply.github.com>
2022-08-29 13:15:57 +0900

    Time: 131 ms (29.64%), Space: 49.1 MB (95.52%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0
+    let right = height.length-1;
+    let result =0;
+    while(left < right){
+        let h = height[left]<height[right]? height[left] : height[right];
+        
+        let area = h*(right-left);
+        if(area>result) result=area;
+        if(height[left]<height[right])left++;
+        else right--;
+    }
+    return result;
+};
