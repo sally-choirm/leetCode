1e621fa35b86d0c48825b1b0d07dea84c93d3891
sally <105703051+sally-choirm@users.noreply.github.com>
2022-09-01 10:38:12 +0900

    Time: 88 ms (64.23%), Space: 44.2 MB (9.39%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+     const queue = [{ node: root, level: 0 }];
+     const result = [];
+     let now = null;
+     while (now = queue.shift()) {
+      if (!now.node) continue;
+        result[now.level] = now.node.val;
+        queue.push({ node: now.node.left, level: now.level + 1 });
+        queue.push({ node: now.node.right, level: now.level + 1 });
+      }
+      return result;
+};
