02adb75fa9e3dc0b7c29419c7d0e70d8f3d48e3f
sally <105703051+sally-choirm@users.noreply.github.com>
2022-09-14 17:35:32 +0900

    Time: 310 ms (5.03%), Space: 51.2 MB (5.65%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let result = new ListNode(0);
+    let link = result;
+    let l1num='';
+    let l2num='';
+    let sum;
+    while(l1!==null){
+        l1num+=l1.val;
+        l1 = l1.next;
+    }
+    while(l2!==null){
+        l2num+=l2.val;
+        l2 = l2.next;
+    }
+    l1num = l1num.split('').reverse().join('');
+    console.log(l1num);
+    l2num = l2num.split('').reverse().join('');
+    console.log(l2num);
+    sum=BigInt(l1num)+BigInt(l2num);
+    sum = sum.toString().split('').reverse();
+    sum.forEach((v)=>{
+        link.next = new ListNode(Number(v));
+        link = link.next;
+    })
+    return result.next;
+}
